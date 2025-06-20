daf5784a8f603e79fe762fbb9d7eb6cbb86db669
Andrey <andrei_novichihin@mail.ru>
2025-06-20 15:24:52 +0300

    pages

--- main/views/bonus.py
+++ main/views/bonus.py
@@ -1,12 +1,19 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView
 
-from main.models import Bonus
+from main.models import Article, Partner, Bonus
 
 
 class BonusList(ListView):
     model = Bonus
     template_name = 'bonus.html'
     context_object_name = 'bonuses'
-    extra_context = {'title': 'Программа лояльности', 'url': reverse_lazy('main:bonus-page')}
+    extra_context = {
+        'title': 'Программа лояльности', 
+        'url': reverse_lazy('main:bonus-page'),
+        'articles': Article.objects.filter(is_deleted=False)[:3],
+        'partners': Partner.objects.filter(is_deleted=False),
+        }
     paginate_by = 12
+
+    
